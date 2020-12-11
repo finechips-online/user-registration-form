fbae0a3b91979d89a6bc6a16333c473f559a3032
Mihir <lucasgreat69@gmail.com>
2020-12-11 15:23:59 +0100

    Add files via upload
    MySql database User registration form

---
+++ reset-password.php
@@ -0,0 +1,14 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+ 
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
